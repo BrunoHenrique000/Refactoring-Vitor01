26b9b44bc51193d6930c53d5894fd65cb473875b
Bruno <bhsilva2@minha.fag.edu.br>
2025-11-25 22:00:50 -0300

    Commit 15
    Commit 15

--- Refactoring/src/Price.java
+++ Refactoring/src/Price.java
@@ -3,9 +3,6 @@ public abstract class Price {
     public abstract double getCharge(int daysRented);
 
     public int getFrequentRenterPoints(int daysRented) {
-        if (getPriceCode() == Movie.NEW_RELEASE) {
-            return 2;
-        }
         return 1;
     }
 }
